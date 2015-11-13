cf575848792d2fcd70966bcf47e8f9aa01edb758
Bryan Ikon <bryan.ikon@gmail.com>
2015-11-13 14:51:41 -0600

    Update upload_kee_pass_kdbx2.rb

--- UploadToDropbox/upload_kee_pass_kdbx2.rb
+++ UploadToDropbox/upload_kee_pass_kdbx2.rb
@@ -2,15 +2,15 @@ require 'dropbox_sdk'
 
 module UploadKeePassKDBX2
 
-  APP_KEY = '5l1ycd1q7yxf6js'
-  APP_SECRET = 'q1040czoixwiqds'
+  APP_KEY = 'key'
+  APP_SECRET = 'secret'
 
   def UploadKeePassKDBX2.connectToDropbox()
     # Get your app key and secret from the Dropbox developer website
 
     flow = DropboxOAuth2FlowNoRedirect.new(APP_KEY, APP_SECRET)
 
-    access_token = 'ADSSQtmZuQAAAAAAAAADiQzLIou8u97h8h8k1SSn5a2hEDo23lSEY5_xevufx9rQ'
+    access_token = 'access_token'
     client = DropboxClient.new(access_token)
   end
 
@@ -23,7 +23,7 @@ client =  UploadKeePassKDBX2.connectToDropbox
 
 puts "linked account:", client.account_info().inspect
 
-file = open('C:\Users\bikonomovski\Documents\BryanIkon.kdbx')
+file = open('path_to_keepass_kdbx')
 response = client.put_file('/backup.kdbx', file)
 
 if response["bytes"] != 0 && response["mime_type"] == "application/octet-stream"
